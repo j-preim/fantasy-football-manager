37493aa56605cb5eb621451879a9750d70de89df
j-preim <j.preimesberger@gmail.com>
2026-08-02 20:11:56 -0500

    position sorting 2

--- src/lib/players/positionStrMapper.ts
+++ src/lib/players/positionStrMapper.ts
@@ -2,21 +2,24 @@
  * Map defaultPositionId -> defaultPositionStr
  */
 export const POSITION_ID_TO_STR: Record<number, string> = {
-  0: "C",
   1: "QB",
   2: "RB",
   3: "WR",
   4: "TE",
   5: "K",
   6: "SS",
-  7: "DL",
-  8: "OF",
+  7: "P",
+  8: "DL",
   9: "OF",
   10: "DE",
-  11: "TQB",
+  11: "LB",
+  12: "DB",
+  13: "DT",
   14: "Coach",
-  15: "RP",
+  15: "TQB",
+  16: "D/ST",
   17: "IL",
+  18: "IR",
 };
 
 // console.log("Position ID to String mapping:", positionStr(1));
